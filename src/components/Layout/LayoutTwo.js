e73e5306f457e549642df306287e0cb35c0e0bb0
gcobastos@gmail.com <gcobastos@gmail.com>
2021-04-16 18:13:29 -0300

    pixel 3 tentativa

--- src/components/Layout/LayoutTwo.js
+++ src/components/Layout/LayoutTwo.js
@@ -18,10 +18,6 @@ export default function LayoutTwo(props) {
   ReactPixel.init('471256527524029', advancedMatching, options);
 
   ReactPixel.pageView(); // For tracking page view
-  ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
-  ReactPixel.trackSingle('471256527524029', event, data); // For tracking default events.
-  ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
-  ReactPixel.trackSingleCustom('471256527524029', event, data); // For tracking custom events.
 
   return (
     <>
