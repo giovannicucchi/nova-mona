30c7384670a35680437963907c4a6d645357c04a
gcobastos@gmail.com <gcobastos@gmail.com>
2021-04-16 18:11:31 -0300

    pixel segunda tentativa

--- src/components/Layout/LayoutTwo.js
+++ src/components/Layout/LayoutTwo.js
@@ -5,29 +5,33 @@ import withScrollFixed from "../../common/withScrollFixed";
 import HeaderTwo from "../Header/HeaderTwo";
 import FooterOne from "../Footer/FooterOne";
 
+import ReactPixel from 'react-facebook-pixel';
+
+
 let ScrollFixedHeader = withScrollFixed(HeaderTwo);
 
 export default function LayoutTwo(props) {
+  const options = {
+    autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+    debug: false, // enable logs
+  };
+  ReactPixel.init('471256527524029', advancedMatching, options);
+
+  ReactPixel.pageView(); // For tracking page view
+  ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
+  ReactPixel.trackSingle('471256527524029', event, data); // For tracking default events.
+  ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
+  ReactPixel.trackSingleCustom('471256527524029', event, data); // For tracking custom events.
+
   return (
     <>
       <Head>
         <title>{props.title || "Loja da Mona"}</title>
         <meta name="facebook-domain-verification" content="xy25p141l9eqommx4yvnykxde1jil6" />
-        <script>
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '471256527524029');
-          fbq('track', 'PageView');
-          </script>
-        <noscript><img height="1" width="1" style="display:none"
+
+        <img height="1" width="1" style="display:none"
           src="https://www.facebook.com/tr?id=471256527524029&ev=PageView&noscript=1"
-        /></noscript>
+        />
       </Head>
       <ScrollFixedHeader container={props.container} />
       {props.children}
